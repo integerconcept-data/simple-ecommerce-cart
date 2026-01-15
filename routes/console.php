634b6d969d2913d18eb3fae9cfb43c4240993e56
integerconcept <sesan2ky@gmail.com>
2026-01-15 02:26:30 +0100

    Add scheduled daily sales report job

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,16 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+use Illuminate\Support\Facades\Schedule;
+use App\Jobs\SendDailySalesReportJob;
+
+
+
+
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::command('report:daily-sales')->dailyAt('18:00');
